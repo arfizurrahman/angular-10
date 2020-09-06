5197e7fc10dca790f332047b84b3b7b36d45ed59
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-09-06 21:16:35 +0600

    Created new project recipe-book

---
+++ angular-complete-guide/src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-success-alert',
+  template: `
+    <p>
+      success-alert works!
+    </p>
+  `,
+  styleUrls: ['./success-alert.component.css']
+})
+export class SuccessAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
