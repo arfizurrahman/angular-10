281375a09f1949d165d8664e07f40302d2f87fad
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-09-08 23:55:20 +0600

    Replaced event emitter with subject

--- recipe-book/src/app/recipes/recipe.service.ts
+++ recipe-book/src/app/recipes/recipe.service.ts
@@ -1,12 +1,10 @@
 import { ShoppingListService } from './../shopping-list/shopping-list.service';
-import { Injectable, EventEmitter } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable()
 export class RecipeService {
-  recipeSelected = new EventEmitter<Recipe>();
-
   private recipes: Recipe[] = [
     new Recipe(
       'Chicken Tikka Masala',
