4519cb71664e193e118ea5cf9ec2603e2e261007
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-09-08 10:22:33 +0600

    Done with services deep dive

---
+++ services/src/app/accounts.service.ts
@@ -0,0 +1,33 @@
+import { LoggingService } from "./loggin.service";
+import { Injectable, EventEmitter } from "@angular/core";
+@Injectable({providedIn: 'root'})
+export class AccountsService {
+  accounts = [
+    {
+      name: "Master Account",
+      status: "active",
+    },
+    {
+      name: "Testaccount",
+      status: "inactive",
+    },
+    {
+      name: "Hidden Account",
+      status: "unknown",
+    },
+  ];
+
+  statusUpdated = new EventEmitter<string>();
+
+  constructor(private logginService: LoggingService) {}
+
+  addAccount(name: string, status: string) {
+    this.accounts.push({ name: name, status: status });
+    this.logginService.logStatusChange(status);
+  }
+
+  updateStatus(id: number, status: string) {
+    this.accounts[id].status = status;
+    this.logginService.logStatusChange(status);
+  }
+}
